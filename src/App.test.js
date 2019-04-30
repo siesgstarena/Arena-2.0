57dea73d07967cf4f0873357874e36186f9714db
ac030540 <ac030540@gmail.com>
2019-04-30 20:49:38 +0530

    Removed a linting issue from App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // eslint-disable-next-line react/jsx-filename-extension
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

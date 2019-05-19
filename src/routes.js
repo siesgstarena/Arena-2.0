db7c97932b37db6f236d41d50a1dd69fd83a0a9f
ac030540 <ac030540@gmail.com>
2019-05-19 11:13:45 +0530

    Updated the routes.js file

--- src/routes.js
+++ src/routes.js
@@ -1,45 +1,31 @@
 // This file contains paths of all the routes and the common components.
 // Paths of the routes changed in this file will update in the overall App.
 
-// The AppBar component is present in src/Components/common/AppBar folder
-export const appBarPath = '/';
-// The components for the home route have not been created yet
-export const homePath = '/';
-// The components for the contests route have not been created yet
-export const contestsPath = '/contests';
-// The components for the ratings route have not been created yet
-export const ratingsPath = '/ratings';
-// The components for the blog route have not been created yet
-export const blogPath = '/blog';
-// The components for the problem-set route have not been created yet
-export const problemSetPath = '/problem-set';
-// The components for the playlists route have not been created yet
-export const playlistsPath = '/playlists';
-// The components for goodies route is present in src/Components/drawer/goodies folder
-export const goodiesPath = '/goodies';
-// The components for the turing-cup route have not been created yet
-export const turingCupPath = '/turing-cup';
-// The components for signin route is present in src/Components/auth/signin folder
+// auth module
 export const signinPath = '/auth/signin';
-// The components for signup route is present in src/Components/auth/signup folder
 export const signupPath = '/auth/signup';
-// The components for forgot route is present in src/Components/auth/forgot folder
 export const forgotPath = '/auth/forgot';
-// The components for reset route is present in src/Components/auth/reset folder
 export const resetPath = '/auth/reset';
-// The components for about route is present in src/Components/footerPages/about folder
-export const aboutPath = '/about';
-// The components for competitions route is present in src/Components/footerPages/competitions
-export const competitionsPath = '/competitions';
-// The components for contact route is present in src/Components/footerPages/contact folder
-export const contactPath = '/contact';
-// The components for developer route is present in src/Components/footerPages/developer folder
-export const developersPath = '/developers';
-// The components for feedback route is present in src/Components/footerPages/feedback folder
-export const feedbackPath = '/feedback';
-// The components for faq route is present in src/Components/footerPages/faq folder
-export const faqPath = '/faq';
-// The components for privacy route is present in src/Components/footerPages/privacy folder
-export const privacyPath = '/privacy';
-// The Footer component is present in src/Components/common/Footer folder
+
+// common module
+export const appBarPath = '/';
 export const footerPath = '/';
+
+// drawer module
+export const homePath = '/';
+export const contestsPath = '/contests';
+export const ratingsPath = '/ratings';
+export const blogPath = '/blog';
+export const problemSetPath = '/problem-set';
+export const playlistsPath = '/playlists';
+export const goodiesPath = '/goodies';
+export const turingCupPath = '/turing-cup';
+
+// footerPages module
+export const aboutPath = '/about';
+export const competitionsPath = '/competitions';
+export const contactPath = '/contact';
+export const developersPath = '/developers';
+export const feedbackPath = '/feedback';
+export const faqPath = '/faq';
+export const privacyPath = '/privacy';

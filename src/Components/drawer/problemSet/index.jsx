a32c4a57e8a2b7150da747da7b847986d9c832f1
ac030540 <ac030540@gmail.com>
2019-07-13 23:02:40 +0530

    Adding linting to the project

--- src/Components/drawer/problemSet/index.jsx
+++ src/Components/drawer/problemSet/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 // import { Headline6, Body1, Body2 } from '@material/react-typography';
 // import { Link } from 'react-router-dom';
 
-const ProblemSet = ({location, history}) => {
-  let params = new URLSearchParams(location.search);
+const ProblemSet = ({ location, history }) => {
+  const params = new URLSearchParams(location.search);
 
   console.log(location);
-  const tags = params.get("tags");
+  const tags = params.get('tags');
   console.log(tags);
   console.log(params);
-  console.log(params.get("newtag"));
+  console.log(params.get('newtag'));
   return (
     <div className="center pa2 mt5 mw7">
       <a className="purple mt0 mb5 tc" href="?tags=adhoc">Playlists Home</a>
@@ -17,22 +17,24 @@ const ProblemSet = ({location, history}) => {
         Hello
       </a>
       {
-        tags===null ?
-        <div>
-        <a href="?tags=adhoc">Adhoc</a>
-        <a href="?tags=string">String</a>
-        </div>
-        : <span />
+        tags === null
+          ? (
+            <div>
+              <a href="?tags=adhoc">Adhoc</a>
+              <a href="?tags=string">String</a>
+            </div>
+          )
+          : <span />
       }
       {
-        tags==='adhoc' ?
-        <p>Adhoc</p>
-        : <span />
+        tags === 'adhoc'
+          ? <p>Adhoc</p>
+          : <span />
       }
       {
-        tags==='string' ?
-        <p>String</p>
-        : <span />
+        tags === 'string'
+          ? <p>String</p>
+          : <span />
       }
     </div>
   );

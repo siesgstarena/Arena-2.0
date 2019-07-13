9fa08daf815f1db881449d0e7a02b74a79f22421
Mithil <mithil467@gmail.com>
2019-07-13 21:56:17 +0530

    Removed extra white space between question and answer

--- src/Components/footerPages/faq/ExpansionPanels.jsx
+++ src/Components/footerPages/faq/ExpansionPanels.jsx
@@ -17,7 +17,6 @@ const styles = () => ({
 
 
 const SimpleExpansionPanel = (props) => {
-  
   const { classes } = props;
   const [expanded, setExpanded] = React.useState(false);
 
@@ -29,10 +28,11 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel  expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}>What is Arena ?</Body1>
+        <Body1 style={{ color: '#1967d2' }} className="mb0">What is Arena ?</Body1>
         </ExpansionPanelSummary>
+        
         <ExpansionPanelDetails>
-          <Body1 className="mid-gray">
+          <Body1 className="mid-gray mt0">
             Arena is a contest platform cum social networking site for people who are
             interested in and want to take part in competitive programming challenges.
             People can organize contests and discuss about other contests.
@@ -43,8 +43,9 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}>What languages are supported and what are its specifications?</Body1>
+          <Body1 style={{ color: '#1967d2' }} className="mb0">What languages are supported and what are its specifications?</Body1>
         </ExpansionPanelSummary>
+        
         <ExpansionPanelDetails>
           <div className="mid-gray">
             Following are the languages and compilers supported by Arena:
@@ -100,10 +101,10 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}> How can I take part in a contest?</Body1>
+          <Body1 style={{ color: '#1967d2' }} className="mb0"> How can I take part in a contest?</Body1>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Body1 className="mid-gray">
+          <Body1 className="mid-gray mt0">
             You can visit any contest from Contests page.
             Open the problem you want to solve and try to
             come up with a solution which will cover all the tasks (example + hidden).
@@ -114,10 +115,10 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}>How can I organize a contest?</Body1>
+          <Body1 style={{ color: '#1967d2' }} className="mb0">How can I organize a contest?</Body1>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Body1 className="mid-gray">
+          <Body1 className="mid-gray mt0">
             You can mail us a request at &nbsp;
             <a className="no-underline mid-gray i dim" href="mailto:codechef@siesgst.ac.in">
               codechef@siesgst.ac.in
@@ -130,10 +131,10 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}>What are ratings?</Body1>
+          <Body1 style={{ color: '#1967d2' }} className="mb0">What are ratings?</Body1>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Body1 className="mid-gray">
+          <Body1 className="mid-gray mt0">
             When the contestants take part in arena contest,
             they may raise or lower their rating that reflects their performance.
             The information about rating changes and its algorithm will be added soon.
@@ -143,10 +144,10 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}>What are blog posts here? Who can write?</Body1>
+          <Body1 style={{ color: '#1967d2' }} className="mb0">What are blog posts here? Who can write?</Body1>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Body1 className="mid-gray">
+          <Body1 className="mid-gray mt0">
             Posts are general blog posts written by any members of this platform.
             These posts can vary in terms of domain and importance.
             We intend to keep the platform clean and as meaningful
@@ -157,14 +158,15 @@ const SimpleExpansionPanel = (props) => {
 
       <ExpansionPanel expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
         <ExpansionPanelSummary expandIcon={<MaterialIcon style={{ color: '#1967d2' }} icon="keyboard_arrow_down" />}>
-          <Body1 style={{ color: '#1967d2' }}> What is the specification of the machine on which code is executed?</Body1>
+          <Body1 style={{ color: '#1967d2' }} className="mb0"> What is the specification of the machine on which code is executed?</Body1>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Body1 className="mid-gray">
+          <Body1 className="mid-gray mt0">
             The Cluster is a Linux Debian Machine with 4 Core AMD CPU and 2GB RAM.
           </Body1>
         </ExpansionPanelDetails>
       </ExpansionPanel>
+
     </div>
   );
 };

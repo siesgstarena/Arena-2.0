b30561052501f71e532d9f9c40ab6427065dae09
Pooja Prasad <poojakprasad94@gmail.com>
2019-10-02 22:07:37 +0530

    add geometry content
    fixes #25

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import PlaylistsWelcomePage from './Components/drawer/playlists/welcomePage/inde
 import PlaylistsHomePage from './Components/drawer/playlists/homePage/index';
 import PlaylistsUNI01 from './Components/drawer/playlists/topicExplanationPage/UNI01';
 import PlaylistsUNI02 from './Components/drawer/playlists/topicExplanationPage/UNI02';
+import PlaylistsUNI05 from './Components/drawer/playlists/topicExplanationPage/UNI05';
 import Goodies from './Components/drawer/goodies/index';
 import Profile from './Components/user/profile/index';
 import Settings from './Components/user/settings/index';
@@ -51,9 +52,26 @@ const App = () => {
             some part of the URL. Hence in our case, AppBar and Footer will be rendered
             on all the pages which has REACT_APP_BASE_ADDRESS in their URL
         */}
-        <Route path="/" render={props => <AppBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} {...props} />} />
-        <Route path="/auth/signin" exact render={props => <SignIn setIsLoggedIn={setIsLoggedIn} {...props} />} />
-        <Route path="/auth/signup" exact render={props => <SignUp setIsLoggedIn={setIsLoggedIn} {...props} />} />
+        <Route
+          path="/"
+          render={props => (
+            <AppBar
+              isLoggedIn={isLoggedIn}
+              setIsLoggedIn={setIsLoggedIn}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          path="/auth/signin"
+          exact
+          render={props => <SignIn setIsLoggedIn={setIsLoggedIn} {...props} />}
+        />
+        <Route
+          path="/auth/signup"
+          exact
+          render={props => <SignUp setIsLoggedIn={setIsLoggedIn} {...props} />}
+        />
         <Route path="/auth/forgot" exact component={Forgot} />
         <Route path="/auth/reset" exact component={Reset} />
         <Route path="/contests/:id" component={ContestTabBar} />
@@ -69,6 +87,7 @@ const App = () => {
         <Route path="/playlists/home" exact component={PlaylistsHomePage} />
         <Route path="/playlists/topic/UNI01" exact component={PlaylistsUNI01} />
         <Route path="/playlists/topic/UNI02" exact component={PlaylistsUNI02} />
+        <Route path="/playlists/topic/UNI05" exact component={PlaylistsUNI05} />
         <Route path="/goodies" exact component={Goodies} />
         <Route path="/profile/:id/settings" exact component={Settings} />
         <Route path="/profile/:id" exact component={Profile} />
@@ -80,10 +99,26 @@ const App = () => {
         <Route path="/faq" exact component={FAQ} />
         <Route path="/privacy" exact component={Privacy} />
         <Route path="/search" exact component={Search} />
-        <Route path="/admin/:contestId/announcements" exact component={AdminEditAnnoucements} />
-        <Route path="/admin/:contestId" exact component={AdminContestDashboard} />
-        <Route path="/admin/:contestId/plagiarism" exact component={AdminPlagiarism} />
-        <Route path="/admin/:contestId/reset/:problemId" exact component={AdminResetSubmissionStatus} />
+        <Route
+          path="/admin/:contestId/announcements"
+          exact
+          component={AdminEditAnnoucements}
+        />
+        <Route
+          path="/admin/:contestId"
+          exact
+          component={AdminContestDashboard}
+        />
+        <Route
+          path="/admin/:contestId/plagiarism"
+          exact
+          component={AdminPlagiarism}
+        />
+        <Route
+          path="/admin/:contestId/reset/:problemId"
+          exact
+          component={AdminResetSubmissionStatus}
+        />
         <Route path="/" component={Footer} />
       </ScrollToTop>
     </BrowserRouter>

96efd308123a2ebaa973a7ee316ee1f01baea1e2
ac030540 <ac030540@gmail.com>
2019-11-11 21:59:22 +0530

    Added the express server file and procfile for deploying on heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT);

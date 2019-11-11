d60e095309a676c60fc550821102a95d6d0e9802
Ashok Choudhary <40123795+ac030540@users.noreply.github.com>
2019-11-11 22:50:55 +0530

    Adding the endpoint for manifest.json

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/manifest.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'manifest.json'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });

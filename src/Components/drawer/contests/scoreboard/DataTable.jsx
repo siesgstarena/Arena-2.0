1fe83d3a416c58da78fe5f951b6f6603d048e44e
Surya Madhavan <32214378+greykoalacode@users.noreply.github.com>
2020-03-22 12:46:37 +0530

    table

--- src/Components/drawer/contests/scoreboard/DataTable.jsx
+++ src/Components/drawer/contests/scoreboard/DataTable.jsx
@@ -1,36 +1,39 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Table from "../../../common/Table/index";
-import Data from "./Data";
-
+import Table from '../../../common/Table/index';
+import Data from './Data';
 
 const DataTable = () => {
-  const tableHeadings = ["#", "Who", "=", "Time"];
-  Data[0].problem.map(prob => {
-    tableHeadings.push(prob.name);
+  const tableHeadings = ['#', 'Who', '=', 'Time'];
+  Data[0].problem.map((prob) => {
+    return tableHeadings.push(prob.name);
   });
 
-  const DataArray = Data.map(entry => {
-    return (
-      <tr key={entry.id} style={{ fontSize: ".9em" }}>
-        <td className="tc pa3">
-          <Link className="no-underline dim blue pointer" to={`submission/${entry.id}`}>
+  const DataArray = Data.map(entry => (
+    <tr key={entry.id} style={{ fontSize: '.9em' }}>
+      <td className="tc pa3">
+        <Link
+          className="no-underline dim blue pointer"
+          to={`submission/${entry.id}`}
+        >
           {entry.id}
-          </Link>
-        </td>
-        <td className="tc pa3">
-          <Link className="no-underline dim blue pointer" to={`/profile/${entry.who.id}`}>
+        </Link>
+      </td>
+      <td className="tc pa3">
+        <Link
+          className="no-underline dim blue pointer"
+          to={`/profile/${entry.who.id}`}
+        >
           {entry.who.username}
-          </Link>
-        </td>
-        <td className="tc pa3">{entry.score}</td>
-        <td className="tc pa3">{entry.time}</td>
-        {entry.problem.map(prob => {
-          return <td className="tc pa3">{prob.score}</td>;
-        })}
-      </tr>
-    );
-  });
+        </Link>
+      </td>
+      <td className="tc pa3">{entry.score}</td>
+      <td className="tc pa3">{entry.time}</td>
+      {entry.problem.map(prob => (
+        <td className="tc pa3">{prob.score}</td>
+      ))}
+    </tr>
+  ));
 
   return (
     <Table

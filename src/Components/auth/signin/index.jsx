f6d50b28d25528a5e48fab219cf27d75ffbd1cbb
ac030540 <ac030540@gmail.com>
2020-03-23 23:21:24 +0530

    Removed useLazyQuery hook as it was not working properly. Will use it once it's working perfectly.

--- src/Components/auth/signin/index.jsx
+++ src/Components/auth/signin/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useLazyQuery } from '@apollo/react-hooks';
+import { useApolloClient } from '@apollo/react-hooks';
 import { Grid, Row, Cell } from '@material/react-layout-grid';
 import TextField, { Input } from '@material/react-text-field';
 import { useHistory, useLocation } from 'react-router-dom';
@@ -36,50 +36,44 @@ const SignIn = () => {
     }
   }, []);
 
-  const [loadUserData, { called, data, error }] = useLazyQuery(
-    GET_USER_DETAILS_ON_LOGIN,
-    { variables: { email, password } },
-  );
 
-
-  // const client = useApolloClient();
+  const client = useApolloClient();
 
   const handleSignIn = async () => {
     setMessageType('loading');
     setMessage('Logging In, Please Wait');
-    loadUserData();
-    // const { data, error } = await client.query({
-    //   query: GET_USER_DETAILS_ON_LOGIN,
-    //   variables: { email, password },
-    // errorPolicy: "all",
-    // onError: ({ networkError, graphqlError }) => {
-    //   console.log(networkError, graphqlError);
-    //   setMessageType('error');
-    //   setMessage('Database error encountered');
-    // },
-    // });
-    if (error && called) {
+    const { data, error } = await client.query({
+      query: GET_USER_DETAILS_ON_LOGIN,
+      variables: { email, password },
+      // errorPolicy: 'none',
+      // onError: ({ networkError, graphqlError }) => {
+      //   console.log(networkError, graphqlError);
+      //   setMessageType('error');
+      //   setMessage('Database error encountered');
+      // },
+    });
+    if (error) {
       // console.log(error.graphQLErrors);
       setMessageType('error');
-      setMessage('Database error encountered.');
+      setMessage('Database error encountered');
       return;
     }
-    if (called) {
-      if (data.login.userId) {
-        setUser({
-          userId: data.login.userId,
-          email: data.login.email,
-          name: data.login.name,
-        });
-        if (state && state.from) {
-          history.push(state.from.pathname);
-          return;
-        }
-        history.push(`/profile/${data.login.userId}`);
-      } else {
-        setMessageType('error');
-        setMessage('Invalid Credentials');
+    if (data.login.userId) {
+      console.log(data);
+      // console.log(data.error);
+      setUser({
+        userId: data.login.userId,
+        email: data.login.email,
+        name: data.login.name,
+      });
+      if (state && state.from) {
+        history.push(state.from.pathname);
+        return;
       }
+      history.push(`/profile/${data.login.userId}`);
+    } else {
+      setMessageType('error');
+      setMessage('Invalid Credentials');
     }
   };
 

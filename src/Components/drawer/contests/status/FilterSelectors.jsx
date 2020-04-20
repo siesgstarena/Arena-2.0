0636f4aa3c8682451c913c29efb46af7a0505671
Surya Madhavan <32214378+greykoalacode@users.noreply.github.com>
2020-04-20 13:53:51 +0530

    Added the filter component in Status Tab
    Added Filter Component in Status Component of Contests tab and it is responsive as floating action button in screen size of Tablet and less than

--- src/Components/drawer/contests/status/FilterSelectors.jsx
+++ src/Components/drawer/contests/status/FilterSelectors.jsx
@@ -1,30 +1,48 @@
 import React, { useState } from 'react';
-import Select, { Option } from '@material/react-select';
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+} from '@material/react-dialog';
+import MaterialIcon from '@material/react-material-icon';
+import Fab from '@material/react-fab';
+import '@material/react-fab/dist/fab.css';
+import '@material/react-dialog/dist/dialog.css';
 import '@material/react-menu-surface/dist/menu-surface.css';
 import '@material/react-menu/dist/menu.css';
 import '@material/react-select/dist/select.css';
+import FilterDialog from './FilterDialog';
+
 
 const FilterSelectors = () => {
-  const [value, setValue] = useState('None');
-  const onEnhancedChange = (index, item) => (
-    setValue(item.getAttribute('data-value'))
-  );
+  const [isOpen, setOpen] = useState(false);
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const screenSize = width < 840;
+
+  window.addEventListener('resize', () => { setWidth(window.innerWidth); });
+
+  if (screenSize) {
+    return (
+      <div>
+        <Fab
+          style={{
+            backgroundColor: '#6200EE', position: 'fixed', right: '2rem', bottom: '2rem',
+          }}
+          icon={(<MaterialIcon icon="filter_list" />)}
+          onClick={() => { setOpen(!isOpen); }}
+        />
+        <Dialog open={isOpen}>
+          <DialogTitle>Filter</DialogTitle>
+          <DialogContent>
+            <FilterDialog isOpen={isOpen} setOpen={setOpen} />
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+  }
   return (
-    <div>
-      <Select
-        className=""
-        notchedOutlineClassName="pa1"
-        enhanced
-        outlined
-        label="Chose"
-        value={value}
-        onEnhancedChange={onEnhancedChange}
-      >
-        <Option value="None">None</Option>
-        <Option value="pomsky">Pomsky</Option>
-        <Option value="goldenDoodle">Golden Doodle</Option>
-      </Select>
-    </div>
+    <FilterDialog setOpen={() => {}} />
   );
 };
 

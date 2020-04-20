2349ac972239c3cb2fbcd69d70a1b8114bdbd713
ac030540 <ac030540@gmail.com>
2020-04-20 22:05:52 +0530

    Updated the class inside EditorContainer to avoid any scss conflicts

--- src/Components/common/MarkdownEditor/EditorContainer.jsx
+++ src/Components/common/MarkdownEditor/EditorContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Body1 } from '@material/react-typography';
-import './EditorContainer.scss';
+import './editorContainer.scss';
 
 const EditorContainer = ({ children, title }) => (
-  <div className="container">
+  <div className="editor-outer-container">
     <Body1 className="ma0 pa0">{title}</Body1>
     {children}
   </div>

5decb44aaa2021fe9676ab4628ca9f2fb75f0f6f
ac030540 <ac030540@gmail.com>
2020-04-20 22:26:00 +0530

    Deleted editorContainer.scss and added the scss of editorContainer directly in App.jsx to avoid any ordering conflicts and thereby avoiding CI build fails.

--- src/Components/common/MarkdownEditor/EditorContainer.jsx
+++ src/Components/common/MarkdownEditor/EditorContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Body1 } from '@material/react-typography';
-import './editorContainer.scss';
 
 const EditorContainer = ({ children, title }) => (
   <div className="editor-outer-container">

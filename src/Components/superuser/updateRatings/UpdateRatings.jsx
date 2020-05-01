6794f933ebdd6eaf99f3be2579d45fb7fc4074a3
ac030540 <ac030540@gmail.com>
2020-05-01 11:36:40 +0530

    Added basic mutation code for updating ratings

--- src/Components/superuser/updateRatings/UpdateRatings.jsx
+++ src/Components/superuser/updateRatings/UpdateRatings.jsx
@@ -1,15 +1,43 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Headline5 } from '@material/react-typography';
-import { useHistory, useParams } from 'react-router-dom';
+// import { useApolloClient } from '@apollo/react-hooks';
+import { useParams } from 'react-router-dom';
 import Button from '@material/react-button';
+import MessageCard from '../../common/MessageCard/index';
 import UpdateRatingsTable from './UpdateRatingsTable';
 import AlertBox from '../../common/AlertBox/index';
 
 const UpdateRatings = ({ ratingsData }) => {
-  const history = useHistory();
   const [isOpen, setIsOpen] = useState(false);
   const { contestId } = useParams();
+  const [messageType, setMessageType] = useState('');
+  const [message, setMessage] = useState('');
+
+  // const client = useApolloClient();
+  const updateDB = async () => {
+    setMessageType('loading');
+    setMessage('Updating ratings, Please wait');
+
+    // const { data, error } = await client.mutate({
+    //   mutation: EDIT_CONTEST,
+    //   variables: {
+    //   },
+    // });
+    // if (error) {
+    //   setMessageType('error');
+    //   setMessage('Database error encountered');
+    //   return;
+    // }
+    // if (data.editContest.success) {
+    //   setMessageType('success');
+    //   setMessage('Ratings updated successfully');
+    // } else {
+    //   setMessageType('error');
+    //   setMessage(data.editContest.message);
+    // }
+  };
+
 
   return (
     <div className="mw7 center ma1 pa2">
@@ -19,6 +47,11 @@ const UpdateRatings = ({ ratingsData }) => {
         {contestId}
       </Headline5>
       <UpdateRatingsTable ratingsData={ratingsData} />
+      <MessageCard
+        messageType={messageType}
+        message={message}
+        setMessageType={setMessageType}
+      />
       <Button
         className="ma1 mt3"
         style={{ marginLeft: 'auto', marginRight: 0, display: 'block' }}
@@ -33,10 +66,8 @@ const UpdateRatings = ({ ratingsData }) => {
         isOpen={isOpen}
         setIsOpen={setIsOpen}
         title="Update Confirmation"
-        content="Are you sure you want to update the ratings?"
-        onAccept={() => {
-          history.push('/superuser/ratings/');
-        }}
+        content={`Are you sure you want to update the ratings for ${contestId}?`}
+        onAccept={updateDB}
       />
     </div>
   );

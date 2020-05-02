75505ede4f2e9510767500e7c4b9b52834c8ae1f
ac030540 <ac030540@gmail.com>
2020-05-02 19:25:28 +0530

    Reduced the padding on the problem set page

--- src/Components/drawer/problemSet/index.jsx
+++ src/Components/drawer/problemSet/index.jsx
@@ -16,7 +16,7 @@ const ProblemSetContainer = () => {
   if (data.problemSet) {
     const problems = data.problemSet;
     return (
-      <div className="mw7 center pa3">
+      <div className="mw7 center">
         <ProblemSet
           problems={problems}
         />

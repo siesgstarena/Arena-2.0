79ea9b1b5e8f1fc3f163c5310056829bced5cff1
ac030540 <ac030540@gmail.com>
2020-05-05 14:02:23 +0530

    Added loading paragraph and loading card components

---
+++ src/Components/common/LoadingParagaph.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+const LoadingParagraph = () => (
+  <div>
+    <Skeleton count={5} />
+    <div className="w-90">
+      <Skeleton />
+    </div>
+    <div className="w-80">
+      <Skeleton />
+    </div>
+    <div className="w-70">
+      <Skeleton />
+    </div>
+  </div>
+);
+
+export default LoadingParagraph;

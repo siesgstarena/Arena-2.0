6d607b7aac719ad71bf6abac73128b75be4a3901
Surya Madhavan <32214378+greykoalacode@users.noreply.github.com>
2020-05-07 20:48:31 +0530

    minor changes

--- src/Components/drawer/blogs/blogPage/BlogFooter/ShareIcon.jsx
+++ src/Components/drawer/blogs/blogPage/BlogFooter/ShareIcon.jsx
@@ -6,7 +6,7 @@ const ShareIcon = () => {
   return (
     <div className="flex mt2">
       <a target="_blank" rel="noopener noreferrer" className="twitter-share-button mr3" href={`https://twitter.com/intent/tweet?url=${url}`}><img alt="tweet" src="https://img.icons8.com/color/32/000000/twitter.png" /></a>
-      <a target="_blank" rel="noopener noreferrer" className="mr3" href={`whatsapp://send?url=${url}`} data-action="share/whatsapp/share"><img alt="whatsapp" src="https://img.icons8.com/office/32/000000/whatsapp.png" /></a>
+      <a target="_blank" rel="noopener noreferrer" className="mr3" href={`whatsapp://send?text=${url}`} data-action="share/whatsapp/share"><img alt="whatsapp" src="https://img.icons8.com/office/32/000000/whatsapp.png" /></a>
       <a target="_blank" rel="noopener noreferrer" className="mr3" href={telegramUrl.concat(url)} data-telegram-share-url="https://core.telegram.org/widgets/share"><img alt="telegram" src="https://img.icons8.com/color/32/000000/telegram-app.png" /></a>
       <a target="_blank" rel="noopener noreferrer" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} className="fb-xfbml-parse-ignore"><img alt="fb" src="https://img.icons8.com/office/32/000000/facebook-new.png" /></a>
     </div>

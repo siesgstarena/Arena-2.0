a7f5c17316a7eb25dd4355f2b21461a1a477300c
ac030540 <ac030540@gmail.com>
2020-05-09 16:35:00 +0530

    Added basic files for converting the App to a PWA

---
+++ public/worker.js
@@ -0,0 +1,48 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = 'your-app-name';
+const urlsToCache = [
+  '/',
+  '/completed',
+];
+
+// Install a service worker
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache worker.js');
+        return cache.addAll(urlsToCache);
+      }),
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }),
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['your-app-name'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.lete(cacheName);
+        }
+      }),
+    )),
+  );
+});

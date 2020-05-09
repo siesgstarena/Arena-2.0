21fdad4c98f7d2b024718983acafc3344e4442e4
ac030540 <ac030540@gmail.com>
2020-05-09 16:08:40 +0530

    Reverted back the spinner to the original one

--- src/Components/common/Spinner/index.jsx
+++ src/Components/common/Spinner/index.jsx
@@ -5,8 +5,8 @@ const Spinner = () => (
   <div>
     <div className="spinner-overlay">
       <div className="spinner-container">
-        <div className="image-spinner" />
-        <img style={{ height: '8em', width: 'auto' }} src="https://res.cloudinary.com/ashokc/image/upload/c_scale,q_10,w_391/v1588856429/arena/assets/arena_wbydxw.jpg" alt="arena" />
+        <div className="spinner" />
+        {/* <img style={{ height: '8em', width: 'auto' }} src="https://res.cloudinary.com/ashokc/image/upload/c_scale,q_10,w_391/v1588856429/arena/assets/arena_wbydxw.jpg" alt="arena" /> */}
       </div>
     </div>
   </div>

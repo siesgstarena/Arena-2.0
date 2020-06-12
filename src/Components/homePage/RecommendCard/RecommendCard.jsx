ac9b934d4f5092c759537c2cfd195641cadaa3af
Surya Madhavan <32214378+greykoalacode@users.noreply.github.com>
2020-06-12 12:35:27 +0530

    small changes

--- src/Components/homePage/RecommendCard/RecommendCard.jsx
+++ src/Components/homePage/RecommendCard/RecommendCard.jsx
@@ -3,32 +3,38 @@ import Card from '@material/react-card';
 import { Headline6, Body1, Headline5 } from '@material/react-typography';
 import './RecommendCard.scss';
 import MaterialIcon from '@material/react-material-icon';
+import { Link } from 'react-router-dom';
 
 const blogData = [
   {
     author: 'Random',
     title: 'Blog 1: Random idea 1 idea 2 idea3 idea4',
     date: 'May 27, 2020',
+    link: '/blogs/5e9418a8d998010022434057',
   },
   {
     author: 'Random',
     title: 'Blog 2: Random idea 2',
     date: 'May 27, 2020',
+    link: '/blogs/5e9418a8d998010022434057',
   },
   {
     author: 'Random',
     title: 'Blog 3: Random idea 3',
     date: 'May 27, 2020',
+    link: '/blogs/5e9418a8d998010022434057',
   },
   {
     author: 'Random',
     title: 'Blog 4: Random idea 4',
     date: 'May 27, 2020',
+    link: '/blogs/5e9418a8d998010022434057',
   },
   {
     author: 'Random',
     title: 'Blog 5: Random idea 5',
     date: 'May 27, 2020',
+    link: '/blogs/5e9418a8d998010022434057',
   },
 ];
 const RecommendCard = () => (
@@ -40,13 +46,15 @@ const RecommendCard = () => (
       </Headline5>
       <div className="recommend">
         {blogData.map((blog) => (
-          <div className="recommend-card" key={blog.title}>
+          <Link to={blog.link} className="recommend-card" key={blog.title}>
+            {/* <div className="recommend-card" key={blog.title}> */}
             <Headline6 className="recommend-card-title">{blog.title}</Headline6>
             <div className="recommend-card-content">
               <Body1 className="ma0">{blog.author}</Body1>
               <span>{blog.date}</span>
             </div>
-          </div>
+            {/* </div> */}
+          </Link>
         ))}
       </div>
     </div>

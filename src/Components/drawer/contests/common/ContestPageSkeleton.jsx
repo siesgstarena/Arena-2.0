0b3fb9cd1d725fd28dd60b0f1978894a8f1f17a0
SalmanAd01 <salmanadhikari415@gmail.com>
2022-12-29 23:19:05 +0530

    fix mobile view

--- src/Components/drawer/contests/common/ContestPageSkeleton.jsx
+++ src/Components/drawer/contests/common/ContestPageSkeleton.jsx
@@ -1,60 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import { Cell } from '@material/react-layout-grid';
 import Split from 'react-split';
+import Fab from '@material/react-fab';
+import MaterialIcon from '@material/react-material-icon';
 import ContestDetails from './ContestDetails';
 import Announcements from './Announcements';
 import Submit from './SubmitOnProblemPage';
 import ContestTabBar from './ContestTabBar';
 import './Split.css';
+import useResizeWindow from '../../../../customHooks/useResizeWindow';
 
 const ContestPageSkeleton = ({ children, contestDetails }) => {
   const { name, description, endsAt, announcement, startsAt } = contestDetails;
+  const [isEditorOpen, setEditorOpen] = useState(false);
+  const width = useResizeWindow();
+  const isMobile = width <= 768;
+  const ChildrenElement = (
+    <React.Fragment key="ChildrenElement">
+      {(!isMobile || !isEditorOpen) && (
+        <Cell
+          desktopColumns={9}
+          tabletColumns={8}
+          style={{
+            overflow: 'hidden',
+          }}
+        >
+          <Cell>
+            <Route path="/contests/:contestId" exact component={ContestTabBar} />
+            <Route path="/contests/:contestId/status" exact component={ContestTabBar} />
+            <Route path="/contests/:contestId/my" exact component={ContestTabBar} />
+            <Route path="/contests/:contestId/scoreboard" exact component={ContestTabBar} />
+            {/* <Route path="/contests/:contestId/submit" exact component={ContestTabBar} /> */}
+            <Route path="/contests/:contestId/change" exact component={ContestTabBar} />
+            <Route path="/contests/:contestId/problem/:problemId" exact component={ContestTabBar} />
+            {children}
+          </Cell>
 
-  return (
-    <Split
-      className="split"
-      dragInterval={1}
-      direction="horizontal"
-      cursor="col-resize"
-      sizes={[35, 65]}
-      minSize={[5, 5]}
-    >
-      <Cell
-        desktopColumns={9}
-        tabletColumns={8}
-        style={{
-          overflow: 'hidden',
-        }}
-      >
-        <Cell>
-          <Route path="/contests/:contestId" exact component={ContestTabBar} />
-          <Route path="/contests/:contestId/status" exact component={ContestTabBar} />
-          <Route path="/contests/:contestId/my" exact component={ContestTabBar} />
-          <Route path="/contests/:contestId/scoreboard" exact component={ContestTabBar} />
-          {/* <Route path="/contests/:contestId/submit" exact component={ContestTabBar} /> */}
-          <Route path="/contests/:contestId/change" exact component={ContestTabBar} />
-          <Route path="/contests/:contestId/problem/:problemId" exact component={ContestTabBar} />
-          {children}
+          <Cell>
+            <ContestDetails
+              name={name}
+              description={description}
+              endsAt={endsAt}
+              startsAt={startsAt}
+            />
+          </Cell>
+          <Cell>
+            <Announcements announcement={announcement} />
+          </Cell>
         </Cell>
+      )}
+      {isMobile && (
+        <Fab
+          textLabel={!isEditorOpen ? 'Editor' : 'Back'}
+          className="center"
+          style={{
+            backgroundColor: '#6200EE',
+            position: 'fixed',
+            bottom: '0.5rem',
+            left: '0.5rem',
+            zIndex: 100,
+          }}
+          icon={<MaterialIcon icon={!isEditorOpen ? 'data_object' : 'arrow_back_ios'} />}
+          onClick={() => setEditorOpen(!isEditorOpen)}
+        />
+      )}
+      {(!isMobile || isEditorOpen) && (
         <Cell>
-          <ContestDetails
-            name={name}
-            description={description}
-            endsAt={endsAt}
-            startsAt={startsAt}
-          />
+          <Submit />
         </Cell>
-        <Cell>
-          <Announcements announcement={announcement} />
-        </Cell>
-      </Cell>
-      <Cell>
-        <Submit />
-      </Cell>
-    </Split>
+      )}
+    </React.Fragment>
   );
+  if (!isMobile) {
+    return (
+      <Split
+        className="split"
+        dragInterval={1}
+        direction="horizontal"
+        cursor="col-resize"
+        sizes={[35, 65]}
+        minSize={[0, 350]}
+      >
+        {[ChildrenElement]}
+      </Split>
+    );
+  }
+  return ChildrenElement;
 };
 
 ContestPageSkeleton.propTypes = {
---
+++ src/customHooks/useResizeWindow.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const useResizeWindow = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+  return width;
+};
+
+export default useResizeWindow;

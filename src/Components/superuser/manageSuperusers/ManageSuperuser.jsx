1e51d3996db164b66a5a49bf639e280f9e2b996f
SalmanAd01 <salmanadhikari415@gmail.com>
2023-01-07 11:00:14 +0530

    improve ui

--- src/Components/superuser/manageSuperusers/ManageSuperuser.jsx
+++ src/Components/superuser/manageSuperusers/ManageSuperuser.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
+import { Box, withStyles, TextField } from '@material-ui/core';
+import { Button } from '@material/react-button';
+import { Headline4 } from '@material/react-typography';
 import { GET_ALL_SUPERUSERS } from '../../../graphql/queries';
 import { MAKE_SUPERUSER } from '../../../graphql/mutations';
 import SomethingWentWrong from '../../common/SomethingWentWrong/index';
 import SuperuserList from './SuperuserList';
 import MessageCard from '../../common/MessageCard';
+import './Style.css';
 
-const ManageSuperuser = () => {
+const styles = () => ({
+  input: {
+    height: 40,
+  },
+  button: {
+    height: 40,
+  },
+});
+const ManageSuperuser = withStyles(styles)((props) => {
   const [username, setUsername] = useState('');
   const [message, setMessage] = useState('');
   const [messageType, setMessageType] = useState('');
@@ -14,6 +26,7 @@ const ManageSuperuser = () => {
   const [makeSuperuser, { data: makeSuperuserData }] = useMutation(MAKE_SUPERUSER, {
     refetchQueries: [{ query: GET_ALL_SUPERUSERS }],
   });
+  const { classes } = props;
   useEffect(() => {
     if (
       makeSuperuserData &&
@@ -39,17 +52,36 @@ const ManageSuperuser = () => {
   if (error) return <SomethingWentWrong message="An error has been encountered." />;
 
   return (
-    <div className="container">
+    <Box className="container">
+      <Headline4 className="purple mt4 mb2 ml0">Manage Superusers</Headline4>
       <SuperuserList users={data.users} />
-      <div className="make-superuser">
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <button type="button" onClick={() => makeSuperuser({ variables: { username } })}>
+      <Box className="make-superuser">
+        <TextField
+          label="username"
+          variant="outlined"
+          className="mr-2 mb-2"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Button
+          outlined
+          color="primary"
+          raised
+          className={classes.button}
+          onClick={() => makeSuperuser({ variables: { username } })}
+        >
           Make Superuser
-        </button>
+        </Button>
         <MessageCard message={message} messageType={messageType} setMessageType={setMessageType} />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
-};
+});
 
 export default ManageSuperuser;
--- src/Components/superuser/manageSuperusers/SuperuserList.jsx
+++ src/Components/superuser/manageSuperusers/SuperuserList.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import { useMutation } from '@apollo/client';
 import PropTypes from 'prop-types';
-import {
-  Paper,
-  TableContainer,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from '@material-ui/core';
+import { Button } from '@material/react-button';
+import Table from '../../common/Table/index';
 import { REMOVE_SUPERUSER } from '../../../graphql/mutations';
 import { GET_ALL_SUPERUSERS } from '../../../graphql/queries';
 import SomethingWentWrong from '../../common/SomethingWentWrong';
@@ -19,47 +12,25 @@ const SuperuserList = ({ users }) => {
     refetchQueries: [{ query: GET_ALL_SUPERUSERS }],
   });
   if (error) return <SomethingWentWrong message="An error has been encountered." />;
+  const tableData = users.map((user) => (
+    <tr key={user.username}>
+      <td>{user.name}</td>
+      <td>{user.username}</td>
+      <td align="right">
+        <Button
+          outline="true"
+          raised
+          color="primary"
+          onClick={() => removeSuperuser({ variables: { username: user.username } })}
+        >
+          Remove
+        </Button>
+      </td>
+    </tr>
+  ));
 
-  return (
-    <div className="superuser-list">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Superusers</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell align="right">Action</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {users &&
-              users.map((user) => (
-                <TableRow
-                  key={user.username}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {user.name}
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {user.username}
-                  </TableCell>
-                  <TableCell align="right">
-                    <button
-                      type="button"
-                      onClick={() => removeSuperuser({ variables: { username: user.username } })}
-                    >
-                      Remove
-                    </button>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+  const tableHeadings = ['Name', 'Username', 'Action'];
+  return <Table tableHeadings={tableHeadings} tableData={tableData} />;
 };
 SuperuserList.propTypes = {
   users: PropTypes.array.isRequired,
